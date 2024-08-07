77c271066cee5512e24e5633ea98a2054cac0ae7
coherencedemo <164892412+coherencedemo@users.noreply.github.com>
2024-08-07 16:08:48 +0200

    Coherence configuration

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,23 @@
-FROM node:18
+# Use an official Node runtime as the base image
+FROM node:18-alpine
+
+# Set the working directory in the container
 WORKDIR /app
-COPY package.json package.json
-COPY package-lock.json package-lock.json
+
+# Copy package.json and package-lock.json
+COPY package*.json ./
+
+# Install dependencies
 RUN npm ci
+
+# Copy the rest of the application code
 COPY . .
+
+# Build the Next.js application
 RUN npm run build
-CMD ["npm", "run", "preview"]
+
+# Expose the port Coherence expects the app to run on
+EXPOSE 8080
+
+# Start the application
+CMD ["npm", "start"]
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'standalone',
+    reactStrictMode: true,
+    swcMinify: true,
+    assetPrefix: process.env.NODE_ENV === 'production' 
+      ? `https://${process.env.CNC_ENVIRONMENT_DOMAIN}` 
+      : '',
+    publicRuntimeConfig: {
+      applicationName: process.env.CNC_APPLICATION_NAME,
+      environmentName: process.env.CNC_ENVIRONMENT_NAME,
+      environmentDomain: process.env.CNC_ENVIRONMENT_DOMAIN,
+      environmentRegion: process.env.CNC_ENVIRONMENT_REGION,
+    },
+    serverRuntimeConfig: {
+      instanceName: process.env.CNC_INSTANCE_NAME,
+    },
+  };
+  
+  export default nextConfig;
