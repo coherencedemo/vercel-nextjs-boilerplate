0989e8d1be0d5fcde10ba029d73d3de4efd8a135
coherencedemo <164892412+coherencedemo@users.noreply.github.com>
2024-08-07 17:24:08 +0200

    Trying fix for Nginx assets

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
     experimental: {
       outputFileTracingRoot: undefined,
     },
+    assetPrefix: process.env.CNC_ENVIRONMENT_DOMAIN ? `https://${process.env.CNC_ENVIRONMENT_DOMAIN}` : undefined,
   };
   
   export default nextConfig;
